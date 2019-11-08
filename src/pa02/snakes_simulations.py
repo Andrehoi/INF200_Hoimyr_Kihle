275924a240082437e113530be09ece309c8ea4db
Sebastian Kihle <sebbek8@hotmail.com>
2019-11-08 13:40:04 +0100

    finished Board class, and started on the player class and subclasses

--- src/pa02/snakes_simulations.py
+++ src/pa02/snakes_simulations.py
@@ -3,6 +3,7 @@
 __author__ = 'sebastian kihle', 'andreas sandvik hoimyr'
 __email__ = 'sebaskih@nmbu.no', 'andrehoi@nmbu.no'
 
+import random
 
 class Board:
 
@@ -22,25 +23,34 @@ class Board:
             return True
         return not self.board[self.goal][1] == 0
 
-    def position_adjustment(self):
+    def position_adjustment(self, position):
+        for chute in self.chutes:
+            if chute[0] == position:
+                return chute[1]
 
-        for pos in self.board:
-
-            if not self.board[pos][1] == 0:
-                if self.board[pos][0] in self.chutes:
-                    self.board[self.chutes[pos][1]][0] = 1
-                    self.board[pos][1] = 0
+        for ladder in self.ladders:
+            if ladder[0] == position:
+                return ladder[1]
+        return position
 
 
+class Player:
+    def __init__(self, board):
+        self.board = board
+        self.position = 0
+
+    def move(self):
+        thorw_die = random.randint(1, 6)
+        self.position += thorw_die
+        self.board.position_adjustment(self.position)
+
+class ResilientPlayer(Player):
+    def __init__(self, board, add_move=1):
+        super().__init__(board)
+        self.add_move = add_move
+
+    def move_resilient(self):
 
 
-"""
-ladders=([1, 40],[9, 11],[37, 53],[44, 63],[50, 80],
-                                [66, 83],[69, 86]),
-                 chutes=([25, 6],[34, 4],[43, 31],[57, 38],[65, 28],[75, 13],
-                         [88,71]), goal=90)
-"""
 
-s = Board()
 
-print(s.board)
